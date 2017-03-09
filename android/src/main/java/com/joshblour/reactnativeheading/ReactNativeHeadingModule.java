5f27959a731ed2b458876c9e7ec884a9329f2297
tangzhibin <zubintang@qq.com>
2017-03-09 13:12:35 +0800

    统一两个平台

--- android/src/main/java/com/joshblour/reactnativeheading/ReactNativeHeadingModule.java
+++ android/src/main/java/com/joshblour/reactnativeheading/ReactNativeHeadingModule.java
@@ -6,10 +6,12 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 
@@ -72,9 +74,11 @@ public class ReactNativeHeadingModule extends ReactContextBaseJavaModule impleme
             return;
         }
 
+        WritableMap heading = Arguments.createMap();
+        heading.putInt("heading", newAzimuth);
         getReactApplicationContext()
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit("headingUpdated", newAzimuth);
+                .emit("headingUpdated", heading);
 
         mAzimuth = newAzimuth;
     }
